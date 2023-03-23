741d9fc8803d8db0be3abf667dc0c927d3debef1
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-03-23 12:07:03 -0600

    Commons Components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,30 @@
 module.exports = {
-    env: {
-        browser: true,
-        amd: true,
-        node: true,
-        es6: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended',
-        'next',
-        'next/core-web-vitals',
-    ],
-    rules: {
-        semi: ['error', 'always'],
-    },
+	env: {
+		browser: true,
+		amd: true,
+		node: true,
+		es6: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:prettier/recommended',
+		'next',
+		'next/core-web-vitals',
+	],
+	rules: {
+		semi: ['error', 'always'],
+		'prettier/prettier': [
+			'error',
+			{
+				semi: true,
+				singleQuote: true,
+				printWidth: 80,
+				tabWidth: 4,
+				useTabs: true,
+				trailingComma: 'es6',
+				bracketSpacing: true,
+			},
+		],
+	},
 };
--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  printWidth: 80,
-  tabWidth: 4,
-  trailingComma: 'es5',
-  bracketSpacing: true,
+	semi: true,
+	singleQuote: true,
+	printWidth: 80,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
 };
