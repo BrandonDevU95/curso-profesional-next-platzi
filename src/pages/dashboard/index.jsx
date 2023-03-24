76be2036e30b27a7d8d5d4c959792c8c30093195
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-03-24 11:31:13 -0600

    Implementación de nuestro componente Chart en el dashboard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,28 +1,50 @@
-const people = [
-	{
-		name: 'Jane Cooper',
-		title: 'Regional Paradigm Technician',
-		department: 'Optimization',
-		role: 'Admin',
-		email: 'jane.cooper@example.com',
-		image:
-			'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-	},
-];
-
+import { Chart } from '@common/Chart';
 import endPoints from '@services/api';
 import useFetch from '@hooks/useFetch';
 
-const PRODUCT_LIMIT = 5;
-const PRODUCT_OFFSET = 5;
+const PRODUCT_LIMIT = 15;
+const PRODUCT_OFFSET = 15;
 
 export default function Dashboard() {
 	const { data } = useFetch(
 		endPoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET)
 	);
 
+	const categoryName = data?.map((product) => product.category);
+	const categoryCount = categoryName?.map((category) => category.name);
+
+	const countOcurrences = (arr) =>
+		arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+
+	const chartData = {
+		datasets: [
+			{
+				label: 'Categories',
+				data: countOcurrences(categoryCount),
+				borderWidth: 2,
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)',
+				],
+				borderColor: [
+					'rgba(255, 99, 132, 1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)',
+				],
+			},
+		],
+	};
+
 	return (
 		<>
+			<Chart className="mb-8 mt-2" chartData={chartData} />
 			<div className="flex flex-col">
 				<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 					<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
