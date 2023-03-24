667dcfe0ebae8b226ad58caf9f02818eaf34a56b
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2023-03-24 09:55:59 -0600

    Guardado del token en una cookie para mantener la sesión

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -31,7 +31,12 @@ function useProvideAuth() {
 			options
 		);
 		if (access_token) {
-			Cookie.set('token', access_token.access_token, { expires: 5 });
+			const token = access_token.access_token;
+			Cookie.set('token', token, { expires: 5 });
+
+			axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+			const { data: user } = await axios.get(endPoints.auth.profile);
+			setUser(user);
 		}
 	};
 
